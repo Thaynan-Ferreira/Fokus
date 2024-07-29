c4d97c3b6fd2bd8ea4beb433cf6053a130b0c248
Thaynan-Ferreira <thaynan_ferreira@outlook.com.br>
2024-07-29 16:39:17 -0300

    Pegando os elementos dinamicos do HTML

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ const html = document.querySelector('html');
 const foco_btn = document.querySelector('.app__card-button--foco');
 const curto_btn = document.querySelector('.app__card-button--curto');
 const longo_btn = document.querySelector('.app__card-button--longo');
+const startPause_btn = document.querySelector('#start-pause')
+
+let tempo = document.querySelector('#timer');
+let imagem = document.querySelector('.app__logo-image');
+let title = document.querySelector('.app__title');
 
 foco_btn.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'foco');
