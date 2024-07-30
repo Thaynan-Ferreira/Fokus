2ca199bc8d57499b003c0ef0d0bdef050dfebd1c
Thaynan-Ferreira <thaynan_ferreira@outlook.com.br>
2024-07-30 09:41:06 -0300

    Adicionando musica ao click do botão

--- main.js
+++ main.js
@@ -2,12 +2,23 @@ const html = document.querySelector('html');
 const foco_btn = document.querySelector('.app__card-button--foco');
 const curto_btn = document.querySelector('.app__card-button--curto');
 const longo_btn = document.querySelector('.app__card-button--longo');
-const startPause_btn = document.querySelector('#start-pause')
-const botoes = document.querySelectorAll('.app__card-button')
+const startPause_btn = document.querySelector('#start-pause');
+const botoes = document.querySelectorAll('.app__card-button');
+const musicaFocoInput = document.querySelector('#alternar-musica');
+const musica = new Audio('/sons/luna-rise-part-one.mp3');
+musica.loop = true;
 
 let tempo = document.querySelector('#timer');
 let banner = document.querySelector('.app__image');
 let title = document.querySelector('.app__title');
+
+musicaFocoInput.addEventListener('change', () => {
+    if(musica.paused){
+        musica.play();
+    } else{
+        musica.pause();
+    }
+})
 
 foco_btn.addEventListener('click', () => {
     alterarContexto('foco');
@@ -26,21 +37,21 @@ longo_btn.addEventListener('click', () => {
 
 function alterarContexto(contexto){
     botoes.forEach(function (contexto){
-        contexto.classList.remove('active')
+        contexto.classList.remove('active');
     })
     html.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `/imagens/${contexto}.png`);
     switch (contexto) {
         case "foco":
-            title.innerHTML = 'Otimize sua produtividade,<br> <strong class="app__title-strong">mergulhe no que importa.</strong>'
+            title.innerHTML = 'Otimize sua produtividade,<br> <strong class="app__title-strong">mergulhe no que importa.</strong>';
 
             break;
         case "descanso-curto":
-            title.innerHTML = 'Que tal dar uma respirada?,<br><strong class="app__title-strong">Faça uma pausa curta!</strong>'
+            title.innerHTML = 'Que tal dar uma respirada?,<br><strong class="app__title-strong">Faça uma pausa curta!</strong>';
             
             break;
         case "descanso-longo":
-            title.innerHTML = 'Hora de voltar à superfície.<br><strong class="app__title-strong">Faça uma pausa longa.</strong>'
+            title.innerHTML = 'Hora de voltar à superfície.<br><strong class="app__title-strong">Faça uma pausa longa.</strong>';
             
             break;
     }
